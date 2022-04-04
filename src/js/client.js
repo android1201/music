45543e03f1b3cf97ebe8ffa9cd4230374d2e71ed
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-04-04 17:45:48 +0000

    Hmm

--- src/js/client.js
+++ src/js/client.js
@@ -38,7 +38,7 @@ const {
 // client vars
 client["discord"] = require('discord.js');
 client["player"] = new Player(client);
-client["queue"] = {};
+client["queue"] = false;
 client["devData"] = {
     commands: new client.discord.Collection(),
     config: config,
